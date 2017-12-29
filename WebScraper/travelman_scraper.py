7bdb2a6d5345517593ab6e9b6d262e8a0a027b9e
TomekFerens <feeroo@gmail.com>
2017-12-29 18:09:43 +0100

    TravelManScraper implementation for travelmaniacy.pl forum

---
+++ WebScraper/travelman_scraper.py
@@ -0,0 +1,110 @@
+from urllib2 import URLError
+import sys
+
+from WebScraper.basic_scraper import BasicScraper
+
+
+"""Wrapper for BasicScraper class.
+Implements functionality with specifics of site travelmaniacy.pl"""
+
+
+class TravelManScraper(BasicScraper):
+    def __init__(self, url):
+        BasicScraper.__init__(self, url)
+
+    """Method responsible for extracting posts content for each topic"""
+    def process_topics(self):
+        # flag indicating if subsequent pages of topic should be processed
+        # (ofc at least should be once)
+        proceed = True
+
+        # max number of failures
+        retry_count = 10
+        posts = []
+        while proceed and retry_count:
+            try:
+                # do processing of topic's threads
+                threads = self._build_link_list('viewtopic')
+                print threads
+                print "pages : {0}".format(str(len(threads)))
+                self.extract_content(threads)
+
+                proceed = self._go_next_page()
+            except URLError:
+                print "Problem with connection"
+                retry_count -= 1
+
+    """Getting main topics as reference point to further processing"""
+    def get_topics(self):
+        return self._build_link_list("viewforum")
+
+    """Builds list of link for desired param, using current soup context"""
+    def _build_link_list(self, param):
+        link_list = []
+        for link in self.soup.find_all('a'):
+            thread = link.get('href')
+            # 'pid' links refer to 'recently added post. ommit them
+            if param in thread and not 'pid' in thread:
+                link_list.append(self.base_url + '/' + thread)
+        return link_list
+
+    """Obtains post content from current page. Iterates recursively over forum (sub-)threads"""
+    def extract_content(self, threads):
+        content = []
+        print "in extract_content"
+        for thread in threads:
+            thread_nr = 0
+            post_nr = 0
+            page_nr = 1
+
+            self.open_web(thread)
+            print "attempt to open website : {0}".format(thread)
+
+            thread_nr += 1
+            # iterate over pages(if not single) and extract content.
+            proceed = True
+            while proceed:
+                # WARNING this size is not truly content size it may be considered as SIZE OF REFERENCES to content
+                # so threshold value is chosen arbitrarily ('real' content reffered - may be even much bigger!)
+                if sys.getsizeof(content) > 1000:
+
+                    print "sizeof content so far : {0} bytes. \n Chunking content...".format(str(sys.getsizeof(content)))
+
+                    self.status_dict["thread_nr"] = thread_nr
+                    self.status_dict["post_nr"] = post_nr
+
+                    self._chunk_file(content, self._generate_chunk_name())
+                    content = []
+
+                for post in self.soup.find_all('div', {'class': 'postmsg'}):
+                    post_nr += 1
+                    print "post  nr : {0}".format(post_nr)
+
+                    post_content = self.clean_html(post.text)
+                    content.append(post_content)
+                    # special char for posts separation and easier parsing
+                    content.append("---------------------------------------------------")
+
+                proceed = self._go_next_page()
+                if proceed:
+                    page_nr += 1
+                    print "proceeding page_nr : {0} of thread...".format(page_nr)
+
+    """Method responsible for possibly moving into next page of current soup context.
+    Note - modifies that context !"""
+    def _go_next_page(self):
+        # get link to next page
+        next_page_links = self._build_link_list('viewforum')
+        # viewforum.php?id = 8 & p = 2
+
+        for link in next_page_links:
+            if '\&p=' in link:
+                next_page_link = link
+                proceed = True
+
+                self.open_web(next_page_link)
+                return proceed  # As soon as we get next_page link return to processing
+            else:
+                # last page of topic or single page of threads(no 'next' button)
+                proceed = False
+        return proceed
--- main_app.py
+++ main_app.py
@@ -1,13 +1,25 @@
 from WebScraper.egory_scraper import EgoryScraper
+from WebScraper.travelman_scraper import TravelManScraper
 
 
 def main():
 
-    base_url = 'http://e-gory.pl/forum'
+    egory = False
+    travelman = True
 
-    ms = EgoryScraper(base_url)
-    threads = ms.get_forum_links()
-    ms.get_posts_content(threads)
+    if egory:
+        base_url = 'http://e-gory.pl/forum'
+
+        ms = EgoryScraper(base_url)
+        threads = ms.get_forum_links()
+        ms.get_posts_content(threads)
+    if travelman:
+        base_url = 'http://travelmaniacy.pl/forum'
+
+        ms = TravelManScraper(base_url)
+        threads = ms.get_forum_links()
+        # print threads
+        ms.get_posts_content(threads)
 
 if __name__ == "__main__":
     main()
