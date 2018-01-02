912ec9b5eee6dc5c9077c7aeb351ded5ab982c55
TomekFerens <feeroo@gmail.com>
2018-01-02 08:40:39 +0100

    Correct of improper doubled install command

--- utils/prep_env.sh
+++ utils/prep_env.sh
@@ -3,6 +3,6 @@
 ###Script responsible for preparing environment to run app"
 
 echo "installing dependencies"
-sudo apt -y install sudo apt install python-bs4 
+sudo apt install python-bs4 
 sudo pip install geojson
 sudo pip install openpyxl
