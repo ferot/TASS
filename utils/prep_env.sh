8a72d17f90477505082c7d4f9a20db578bbf4bb0
TomekFerens <feeroo@gmail.com>
2018-01-11 15:11:56 +0100

    Networkx 2.0 support

--- utils/prep_env.sh
+++ utils/prep_env.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 ###Script responsible for preparing environment to run app"
+sudo pip install --upgrade pip
 
 echo "installing dependencies"
 sudo apt install python-bs4 
+sudo pip install networkx
 sudo pip install geojson
 sudo pip install openpyxl
 sudo pip install unidecode
